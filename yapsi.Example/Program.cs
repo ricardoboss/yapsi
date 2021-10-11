b019ba28d2e775b5f50c3ea710aab03622039a6f
Ricardo Boss <contact@ricardoboss.de>
2021-10-12 01:03:17 +0200

    Added example project

---
+++ yapsi.Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using yapsi.Default;
+
+// create a pipeline somewhere and make it available (through dependency injection for example)
+var pipeline = new Pipeline<string>();
+
+// ...somewhere you want to provide data from
+var contract = pipeline.Bind();
+
+// ...somewhere else where you want to consume the data
+var subscription = pipeline.Subscribe();
+subscription.Published += (sender, message) => Console.WriteLine(message);
+
+// publish some data
+contract.Publish("Hello from the other side");
+contract.Publish("Hello again");
