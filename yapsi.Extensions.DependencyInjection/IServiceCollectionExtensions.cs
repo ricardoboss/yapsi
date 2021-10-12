6ca14be58625625bfae474ea59c9ab0f33c204e0
Ricardo Boss <contact@ricardoboss.de>
2021-10-12 02:51:22 +0200

    Added yapsi.Extensions.DependecyInjection project

---
+++ yapsi.Extensions.DependencyInjection/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using yapsi.Default;
+
+namespace yapsi.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddYapsi<P, T>(this IServiceCollection serviceProvider) where P : Pipeline<T>
+        {
+            return serviceProvider
+                .AddSingleton<P>()
+                .AddTransient(sp => sp.GetRequiredService<P>().Subscribe())
+                .AddTransient(sp => sp.GetRequiredService<P>().Bind());
+        }
+    }
+}
